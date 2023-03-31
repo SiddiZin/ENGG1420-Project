d4d97c3f61c2c25686971c712df671612f6cc23d
Zain <zainsiddiqui843@gmail.com>
2023-03-30 21:57:41 -0400

    Here is my error free code version for this Labtest

---
+++ LaserFicheProject/src/laserficheproject/LaserFicheProject.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package laserficheproject;
+
+/**
+ *
+ * @author SidZa
+ */
+public class LaserFicheProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
